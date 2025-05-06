f3737c65ea4550de0672b966dbedd9f5bb3fa899
FenodyreeAv <fenodyree.av@gmail.com>
2025-05-06 21:10:55 +0100

    Finished

--- Assets/Scripts/SocketBleed.cs
+++ Assets/Scripts/SocketBleed.cs
@@ -20,20 +20,30 @@ public class SocketBleed : MonoBehaviour
                 organContactCoroutine = StartCoroutine(HandleOrganContact(other));
             }
         }
-        else
+        if (other.CompareTag("GrabbableObject"))
         {
-            if (!isBleeding)
+
+            if (other.name == "Saw" || other.name == "Scalpel" || other.name == "Hammer")
             {
-                Debug.Log("Bleeding started");
-                isBleeding = true;
-                bleedCoroutine = StartCoroutine(SpawnBleedParticles());
+                if (!isBleeding)
+                {
+                    Debug.Log("Bleeding started");
+                    isBleeding = true;
+                    bleedCoroutine = StartCoroutine(SpawnBleedParticles());
+                }
+                Quaternion randomRotation = Random.rotation;
+                GameObject particleInstance = Instantiate(socketBleedParticleSystem, transform.position, randomRotation);
+                particleInstance.transform.SetParent(transform.parent.parent, true);
+                instantiatedBleeds.Add(particleInstance);
             }
-            Quaternion randomRotation = Random.rotation;
-            GameObject particleInstance = Instantiate(socketBleedParticleSystem, transform.position, randomRotation);
-            particleInstance.transform.SetParent(transform.parent.parent, true);
-            instantiatedBleeds.Add(particleInstance);
+            else if (other.transform.parent != null && other.transform.parent.CompareTag("NewOrgan"))
+            {
+                Debug.Log("GrabbableObject is a NewOrgan");
+                organContactCoroutine = StartCoroutine(HandleOrganContact(other.transform.parent.GetComponent<Collider>()));
+            }
         }
     }
+
 
     private void OnTriggerExit(Collider other)
     {
@@ -53,9 +63,6 @@ public class SocketBleed : MonoBehaviour
     private IEnumerator HandleOrganContact(Collider organ)
     {
         yield return new WaitForSeconds(1f);
-
-        if (organ != null && organ.CompareTag("NewOrgan"))
-        {
             Debug.Log("Deleting instantiated bleeds due to NewOrgan contact");
             foreach (var bleed in instantiatedBleeds)
             {
@@ -71,7 +78,6 @@ public class SocketBleed : MonoBehaviour
                 organRigidbody.isKinematic = true;
                 Debug.Log("Organ's Rigidbody is now kinematic");
             }
-        }
     }
 
     private IEnumerator SpawnBleedParticles()
